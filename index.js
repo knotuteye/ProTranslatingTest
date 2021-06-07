9e3c567038a25a2f910a25f0922b46f0a826419d
Kevin Otuteye <kevinotuteye@gmail.com>
2021-06-07 01:54:15 +0000

    added Swagger

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const router = require("./router");
 const app = express();
 const port = 3000;
 
+const swaggerJsdoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
+const swaggerJSDoc = require("swagger-jsdoc");
+const swaggerConfig = require("./swagger.json");
+const specs = swaggerJSDoc(swaggerConfig);
+
 app.use(router);
 
 app.listen(port, () => {
