9f9c39f0339fce0e7fef1388829c5d4040a8d9f7
Kevin Otuteye <kevinotuteye@gmail.com>
2021-06-07 21:14:30 +0000

    began toolbar

---
+++ client/src/components/Table.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <div class="flex flex-row justify-between px-8 py-5 bg-blue-100">
+      <h1 class="text-2xl font-bold text-blue-800">Clients</h1>
+      <button class="bg-gray-100 border-gray-200 border text-sm px-5">New Client</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+</style>
