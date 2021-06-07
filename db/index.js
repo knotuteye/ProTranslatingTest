4cbf64eb7fbca262826c7b358ca88d0306d92c88
Kevin Otuteye <kevinotuteye@gmail.com>
2021-06-07 12:24:35 +0000

    added dotenv

--- db/index.js
+++ db/index.js
@@ -1,3 +1,3 @@
+require("dotenv").config();
 const mongoose = require("mongoose");
-const connectionString =
-  "mongodb+srv://maintainer:maintainer123@cluster0.1quzr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const connectionString = process.env["DB_STRING"];
