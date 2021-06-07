7131653de86fc247ed91b6139aad2de7d48faaee
Kevin Otuteye <kevinotuteye@gmail.com>
2021-06-07 01:42:36 +0000

    added Readme

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = require("./router");
+const app = express();
+const port = 3000;
+
+app.use(router);
+
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+});
