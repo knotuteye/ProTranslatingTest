4522365a10f3d11dc0d392ccb20869f20710a785
Kevin Otuteye <kevinotuteye@gmail.com>
2021-06-07 16:39:15 +0000

    added prettier config and example .env

--- db/index.js
+++ db/index.js
@@ -1,3 +1,17 @@
-require("dotenv").config();
-const mongoose = require("mongoose");
-const connectionString = process.env["DB_STRING"];
+require('dotenv').config()
+const mongoose = require('mongoose')
+const connectionString = process.env['DB_STRING']
+
+mongoose.connect(connectionString, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+
+const db = mongoose.connection
+
+db.on(
+  'error',
+  console.error.bind(console, 'ERROR: Could not connect to database')
+)
+
+db.once('open', () => console.log('Connected to database successfuly'))
